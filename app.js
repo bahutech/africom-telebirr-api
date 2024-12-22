1703ffd90ab5a044989351d0e1b72ff9a6d9af4d
bahutech <54044336+bahutech@users.noreply.github.com>
2024-12-23 02:12:08 +0300

    start

--- app.js
+++ app.js
@@ -276,7 +276,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // allow cross-origin
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*,*");
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
