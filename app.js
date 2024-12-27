c934b260b6120693693621c11730b10cef304f00
bahutech <54044336+bahutech@users.noreply.github.com>
2024-12-27 05:00:31 +0300

    order asc

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ function getCustomerDetail(id) {
 function getProducts(page, per_page) {
   return new Promise((resolve, reject) => {
     var options = {
-      url: `https://aliexpress.andagna.com/wp-json/wc/v3/products?per_page=${per_page}&page=${page}&orderby=${'price'}`,
+      url: `https://aliexpress.andagna.com/wp-json/wc/v3/products?per_page=${per_page}&page=${page}&orderby=${'price'}&order=${'asc'}`,
       auth: {
           'user': 'ck_f698c13cdafb13d2dcf2ba42257522537bdff188',
           'pass': 'cs_97eb40b7df1e997925d5b9181f6ac080a308ac95'
